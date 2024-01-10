1ad72708e5090b3bf26156b5608a6558936bacfb
chloebweaver <132216902+chloebweaver@users.noreply.github.com>
2024-01-10 14:53:08 -0600

    Update repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+function repeatString = function(string, num) {
+    for (let i = 0; i < num; i++) {
+        return(string);
+    }
 };
 
 // Do not edit below this line
